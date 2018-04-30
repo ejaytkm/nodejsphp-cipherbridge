1ec67b4fc71c3857d158fba408d1916250bdd193
Ejay Tan Mac <ejaytkm@gmail.com>
2018-04-30 16:24:31 +0800

    Body into stringify version

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const CryptoJS = require('crypto-js')
 function CryptoJSAesEncrypt (passphrase, plainText) {
   const salt = CryptoJS.lib.WordArray.random(256)
   const iv = CryptoJS.lib.WordArray.random(16)
-  const key = CryptoJS.PBKDF2(passphrase, salt, { hasher: CryptoJS.algo.SHA512, keySize: 64 / 8, iterations: 999 })
+  const key = CryptoJS.PBKDF2(passphrase, salt, {
+    hasher: CryptoJS.algo.SHA512,
+    keySize: 64 / 8,
+    iterations: 999
+  })
   const encrypted = CryptoJS.AES.encrypt(plainText, key, {iv: iv})
   const data = {
     ciphertext: CryptoJS.enc.Base64.stringify(encrypted.ciphertext),
@@ -12,7 +16,7 @@ function CryptoJSAesEncrypt (passphrase, plainText) {
   }
   return data
 }
-module.exports = (uri, publicKey, privateKey) => {
+module.exports = (uri, publicKey, privateKey, extra = null) => {
   return {
     send: async (data) => {
       const encrypted = CryptoJSAesEncrypt(privateKey, data)
@@ -20,12 +24,13 @@ module.exports = (uri, publicKey, privateKey) => {
         uri: uri,
         method: 'POST',
         json: true,
-        body: {
+        body: JSON.stringify({
           ciphertext: encrypted.ciphertext,
           salt: encrypted.salt,
           iv: encrypted.iv,
-          public_key: publicKey
-        }
+          public_key: publicKey,
+          extra: extra
+        })
       })
       return response
     }
